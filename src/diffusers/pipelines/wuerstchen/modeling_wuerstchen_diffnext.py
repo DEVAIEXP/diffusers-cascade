8b519ac53bfae7d3b8fed6bf78aff960343a3cd6
Eliseu Silva <elismasilva@gmail.com>
2024-03-16 00:04:33 -0300

    Fixed FP16

--- src/diffusers/pipelines/wuerstchen/modeling_wuerstchen_diffnext.py
+++ src/diffusers/pipelines/wuerstchen/modeling_wuerstchen_diffnext.py
@@ -244,7 +244,7 @@ class ResBlockStageB(nn.Module):
             nn.Dropout(dropout),
             nn.Linear(c * 4, c),
         )
-
+    @torch.cuda.amp.autocast(dtype=torch.float32)
     def forward(self, x, x_skip=None):
         x_res = x
         x = self.norm(self.depthwise(x))
